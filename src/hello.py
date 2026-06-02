9afb03a45df3e68c143b44a8a523fdae46bf6c6b
skitzer <skitzer@student.tgm.ac.at>
2026-06-02 17:03:54 +0200

    swapped spencer for samo in python container

--- src/hello.py
+++ src/hello.py
@@ -15,7 +15,7 @@ def hello_spencer():
     f.close()
 
     return jsonify({
-        "message": "Hello Spencer",
+        "message": "Hello Samo",
         "counter" : counter,
         "status": "success"
     })
